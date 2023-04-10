ca67d365936994592257d3a12a6f977b27de388d
bam-e22 <stack.07142@gmail.com>
2023-04-10 14:53:14 +0900

    add onClick method to FormButton

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -3,16 +3,14 @@ import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-import 'email_screen.dart';
-
-class UsernameScreen extends StatefulWidget {
-  const UsernameScreen({Key? key}) : super(key: key);
+class EmailScreen extends StatefulWidget {
+  const EmailScreen({Key? key}) : super(key: key);
 
   @override
-  State<UsernameScreen> createState() => _UsernameScreenState();
+  State<EmailScreen> createState() => _EmailScreenState();
 }
 
-class _UsernameScreenState extends State<UsernameScreen> {
+class _EmailScreenState extends State<EmailScreen> {
   final TextEditingController _usernameController = TextEditingController();
   String _username = "";
 
@@ -32,14 +30,6 @@ class _UsernameScreenState extends State<UsernameScreen> {
     super.dispose();
   }
 
-  void _onNextTap(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => const EmailScreen(),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,19 +47,10 @@ class _UsernameScreenState extends State<UsernameScreen> {
           children: [
             Gaps.v40,
             const Text(
-              "Create username",
+              "What is your email?",
               style: TextStyle(
                 fontSize: Sizes.size24,
                 fontWeight: FontWeight.w700,
-              ),
-            ),
-            Gaps.v8,
-            const Text(
-              "You can always change this later.",
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w600,
               ),
             ),
             Gaps.v16,
@@ -77,7 +58,7 @@ class _UsernameScreenState extends State<UsernameScreen> {
               controller: _usernameController,
               cursorColor: Theme.of(context).primaryColor,
               decoration: InputDecoration(
-                hintText: "Username",
+                hintText: "Email",
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(
                     color: Colors.grey.shade400,
@@ -91,10 +72,7 @@ class _UsernameScreenState extends State<UsernameScreen> {
               ),
             ),
             Gaps.v16,
-            FormButton(
-              enabled: _username.isNotEmpty,
-              onClick: (context) => _onNextTap(context),
-            )
+            FormButton(enabled: _username.isNotEmpty)
           ],
         ),
       ),
--- lib/features/authentication/widgets/form_button.dart
+++ lib/features/authentication/widgets/form_button.dart
@@ -4,33 +4,38 @@ import '../../../constants/sizes.dart';
 
 class FormButton extends StatelessWidget {
   final bool enabled;
+  final void Function(BuildContext context)? onClick;
 
   const FormButton({
     super.key,
     required this.enabled,
+    this.onClick,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: AnimatedContainer(
-        padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
-        decoration: BoxDecoration(
-          color:
-              enabled ? Theme.of(context).primaryColor : Colors.grey.shade300,
-          borderRadius: BorderRadius.circular(Sizes.size5),
-        ),
-        duration: const Duration(milliseconds: 200),
-        child: AnimatedDefaultTextStyle(
-          duration: const Duration(milliseconds: 200),
-          style: TextStyle(
-            color: enabled ? Colors.white : Colors.grey.shade400,
-            fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () => enabled ? onClick?.call(context) : null,
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: AnimatedContainer(
+          padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
+          decoration: BoxDecoration(
+            color:
+                enabled ? Theme.of(context).primaryColor : Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(Sizes.size5),
           ),
-          child: const Text(
-            'Next',
-            textAlign: TextAlign.center,
+          duration: const Duration(milliseconds: 200),
+          child: AnimatedDefaultTextStyle(
+            duration: const Duration(milliseconds: 200),
+            style: TextStyle(
+              color: enabled ? Colors.white : Colors.grey.shade400,
+              fontWeight: FontWeight.w600,
+            ),
+            child: const Text(
+              'Next',
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
