41324f19ee91851bbeb48549514db02eeeb161c0
bam-e22 <stack.07142@gmail.com>
2023-04-19 22:17:47 +0900

    navigate LoginScreen to LoginFormScreen

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class LoginFormScreen extends StatelessWidget {
+import '../../constants/sizes.dart';
+
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({Key? key}) : super(key: key);
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size40,
+        ),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(),
+              TextFormField(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
