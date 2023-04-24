efada9972e1d9ae2e262f08e3eff58b376ae21c2
bam-e22 <stack.07142@gmail.com>
2023-04-24 22:35:37 +0900

    add VideoPost

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -39,6 +39,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
           Offstage(
--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({Key? key}) : super(key: key);
@@ -12,31 +11,36 @@ class VideoTimelineScreen extends StatefulWidget {
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   int _itemCount = 4;
   final PageController _pageController = PageController();
-  final List<Color> _colors = [
-    Colors.blue,
-    Colors.teal,
-    Colors.yellow,
-    Colors.amber
-  ];
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
 
   void _onPageChanged(int page) {
     // TODO: 강제로 animate 하지 말고 pageView 스펙 자체를 변경할 수는 없을까?
     _pageController.animateToPage(
       page,
-      duration: const Duration(milliseconds: 150),
-      curve: Curves.linear,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
     );
 
     var moreItemCount = 4;
     if (page == _itemCount - 1) {
-      for (var i = 0; i < moreItemCount; i++) {
-        _colors.add(
-            Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0));
-      }
       setState(() {
         _itemCount += moreItemCount;
       });
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -46,16 +50,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       scrollDirection: Axis.vertical,
       itemCount: _itemCount,
       onPageChanged: _onPageChanged,
-      itemBuilder: (context, index) => Container(
-        color: _colors[index],
-        child: Center(
-          child: Text(
-            '$index',
-            style: TextStyle(
-              fontSize: 48,
-            ),
-          ),
-        ),
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
       ),
     );
   }
