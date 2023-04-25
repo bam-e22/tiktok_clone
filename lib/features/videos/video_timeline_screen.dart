1b6de31ec471a45470503a198f108fe2cc06cedc
bam-e22 <stack.07142@gmail.com>
2023-04-25 17:39:49 +0900

    refresh indicator

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -38,6 +38,13 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     );*/
   }
 
+  Future<void> _onRefresh() {
+    return Future.delayed(
+      // Fake api call
+      const Duration(seconds: 2),
+    );
+  }
+
   @override
   void dispose() {
     _pageController.dispose();
@@ -46,13 +53,19 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: _pageController,
-      scrollDirection: Axis.vertical,
-      itemCount: _itemCount,
-      onPageChanged: _onPageChanged,
-      itemBuilder: (context, index) =>
-          VideoPost(onVideoFinished: _onVideoFinished, index: index),
+    return RefreshIndicator(
+      displacement: 50,
+      edgeOffset: 20,
+      color: Theme.of(context).primaryColor,
+      onRefresh: _onRefresh,
+      child: PageView.builder(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        itemCount: _itemCount,
+        onPageChanged: _onPageChanged,
+        itemBuilder: (context, index) =>
+            VideoPost(onVideoFinished: _onVideoFinished, index: index),
+      ),
     );
   }
 }
