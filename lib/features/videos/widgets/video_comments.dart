110e7f169ce9b8c5e95051d61b4693e6d83ba8fe
bam-e22 <stack.07142@gmail.com>
2023-04-25 23:05:20 +0900

    add bottomSheet comment

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+import '../../../constants/gaps.dart';
 
 class VideoComments extends StatefulWidget {
   const VideoComments({Key? key}) : super(key: key);
@@ -32,10 +35,72 @@ class _VideoCommentsState extends State<VideoComments> {
           ),
         ],
       ),
-      body: ListView.builder(
+      body: ListView.separated(
+        padding: EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+          horizontal: Sizes.size16,
+        ),
         itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          child: const Text("I'm a comment"),
+        separatorBuilder: (context, index) => Gaps.v20,
+        itemBuilder: (context, index) {
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text("todd"),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'User',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Gaps.v3,
+                    Text(
+                        "add showModelBottomSheet and apply annnn border radius."),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    '52.2K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  )
+                ],
+              )
+            ],
+          );
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 18,
+              backgroundColor: Colors.grey.shade500,
+              foregroundColor: Colors.white,
+              child: Text("todd"),
+            )
+          ],
         ),
       ),
     );
