49572b61cc09c09629220e2dd353313b00d824bf
bam-e22 <stack.07142@gmail.com>
2023-04-27 13:47:28 +0900

    add TabBar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/discover/discover_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
@@ -15,7 +16,8 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
+  //int _selectedIndex = 0;
+  int _selectedIndex = 1; // TODO: for test
 
   void _onTap(int index) {
     setState(() {
@@ -49,9 +51,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           ),
           Offstage(
             offstage: _selectedIndex != 1,
-            child: Container(
-              color: Colors.white,
-            ),
+            child: DiscoverScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 3,
