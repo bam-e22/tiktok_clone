632870c1a68bd1718c8b490ff45a1fde8d3b4f15
bam-e22 <stack.07142@gmail.com>
2023-05-04 11:07:19 +0900

    request permissions and initialize camera

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,10 +1,99 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
-class VideoRecordingScreen extends StatelessWidget {
+import '../../constants/gaps.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({Key? key}) : super(key: key);
 
   @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _isLoading = true;
+  bool _hasPermission = false;
+  late final CameraController _cameraController;
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final isCameraPermissionGranted = cameraPermission.isGranted &&
+        !(cameraPermission.isDenied || cameraPermission.isPermanentlyDenied);
+    final isMicPermissionGranted = micPermission.isGranted &&
+        !(micPermission.isDenied || micPermission.isPermanentlyDenied);
+
+    if (isCameraPermissionGranted && isMicPermissionGranted) {
+      _hasPermission = true;
+      await initCamera();
+    } else {
+      _hasPermission = false;
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) return;
+
+    _cameraController =
+        CameraController(cameras[0], ResolutionPreset.ultraHigh);
+
+    await _cameraController.initialize();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox.expand(
+        child: _isLoading
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Initializing...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              )
+            : _hasPermission && _cameraController.value.isInitialized
+                ? Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      CameraPreview(_cameraController),
+                    ],
+                  )
+                : Center(
+                    child: Text(
+                      _hasPermission
+                          ? "Permission denied"
+                          : "Camera error occurred",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ),
+      ),
+    );
   }
 }
